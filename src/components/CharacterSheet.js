03b7b4f5ee5f7d60405e9856b88a7c8488c29928
BobesCZ <bobes.chaos@gmail.com>
2018-11-25 01:06:10 +0100

    health table in character sheet

--- src/components/CharacterSheet.js
+++ src/components/CharacterSheet.js
@@ -14,126 +14,187 @@ const mapStateToProps = (state) => {
   };
 };
 
-const ConnectedSheets = ({info, abilities, derivedAbilities, combatParameters}) => (
-  <div className="character-sheet panel panel-default">
-    <div className="panel-body">
-      <h2>
-        {info.get('name')}
-      </h2>
-      <h3>
-        {translations[info.get('race')]}
-        &nbsp;-&nbsp;
-        {translations[info.get('class')]}&nbsp;
-        ({info.get('level')}.&nbsp;{translations.levelLow})
-      </h3>
-      <p>
-        {info.get('note')}
-      </p>
+class ConnectedSheets extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {};
+  }
 
-      <div className="row">
+  render(props) {
+    let healthArray = [];
+    let charHealth = this.props.combatParameters.get('health');
 
-        <div className="col-xs-4">
-          <table className="table ability-table">
-            <tbody>
-              <tr>
-                <td>{translations.strength}</td>
-                <td>{abilities.get('strength')}</td>
-              </tr>
-              <tr>
-                <td>{translations.dexterity}</td>
-                <td>{abilities.get('dexterity')}</td>
-              </tr>
-              <tr>
-                <td>{translations.manualdexterity}</td>
-                <td>{abilities.get('manualdexterity')}</td>
-              </tr>
-              <tr>
-                <td>{translations.will}</td>
-                <td>{abilities.get('will')}</td>
-              </tr>
-              <tr>
-                <td>{translations.intelligence}</td>
-                <td>{abilities.get('intelligence')}</td>
-              </tr>
-              <tr>
-                <td>{translations.charisma}</td>
-                <td>{abilities.get('charisma')}</td>
-              </tr>
-            </tbody>
-          </table>
+    for (var i=1; i <= 50; i++) {
+      let active = i <= charHealth ? true : false;
+      let content = "";
+
+      // Print content to table cell for number 1, number corresponding to character health value and each 5 cells
+      if (
+        i === 1 ||
+        i % 5 === 0 ||
+        i === charHealth
+      )
+      {
+        content = i;
+      }
+      healthArray[i] = {"active": active, "content": content};
+    }
+
+    let healthRowArray = [];
+
+    for (var i=0; i < 3; i++) {
+      healthRowArray.push("healthRow" + i);
+    }
+
+    return (
+      <div className="character-sheet panel panel-default">
+        <div className="panel-body">
+          <h2>
+            {this.props.info.get('name')}
+          </h2>
+          <h3>
+            {translations[this.props.info.get('race')]}
+            &nbsp;-&nbsp;
+            {translations[this.props.info.get('class')]}&nbsp;
+            ({this.props.info.get('level')}.&nbsp;{translations.levelLow})
+          </h3>
+          <p>
+            {this.props.info.get('note')}
+          </p>
+
+          <div className="row">
+
+            <div className="col-xs-4">
+              <table className="table ability-table">
+                <tbody>
+                  <tr>
+                    <td>{translations.strength}</td>
+                    <td>{this.props.abilities.get('strength')}</td>
+                  </tr>
+                  <tr>
+                    <td>{translations.dexterity}</td>
+                    <td>{this.props.abilities.get('dexterity')}</td>
+                  </tr>
+                  <tr>
+                    <td>{translations.manualdexterity}</td>
+                    <td>{this.props.abilities.get('manualdexterity')}</td>
+                  </tr>
+                  <tr>
+                    <td>{translations.will}</td>
+                    <td>{this.props.abilities.get('will')}</td>
+                  </tr>
+                  <tr>
+                    <td>{translations.intelligence}</td>
+                    <td>{this.props.abilities.get('intelligence')}</td>
+                  </tr>
+                  <tr>
+                    <td>{translations.charisma}</td>
+                    <td>{this.props.abilities.get('charisma')}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
+            <div className="col-xs-4">
+              <table className="table ability-table">
+                <tbody>
+                  <tr>
+                    <td>{translations.resistance}</td>
+                    <td>{this.props.derivedAbilities.get('resistance')}</td>
+                  </tr>
+                  <tr>
+                    <td>{translations.fortitude}</td>
+                    <td>{this.props.derivedAbilities.get('fortitude')}</td>
+                  </tr>
+                  <tr>
+                    <td>{translations.speed}</td>
+                    <td>{this.props.derivedAbilities.get('speed')}</td>
+                  </tr>
+                  <tr>
+                    <td>{translations.senses}</td>
+                    <td>{this.props.derivedAbilities.get('senses')}</td>
+                  </tr>
+                  <tr>
+                    <td>{translations.beauty}</td>
+                    <td>{this.props.derivedAbilities.get('beauty')}</td>
+                  </tr>
+                  <tr>
+                    <td>{translations.danger}</td>
+                    <td>{this.props.derivedAbilities.get('danger')}</td>
+                  </tr>
+                  <tr>
+                    <td>{translations.dignity}</td>
+                    <td>{this.props.derivedAbilities.get('dignity')}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
+            <div className="col-xs-4">
+              <table className="table ability-table">
+                <tbody>
+                  <tr>
+                    <td>{translations.combatSpeed}</td>
+                    <td>{this.props.combatParameters.get('combatSpeed')}</td>
+                  </tr>
+                  <tr>
+                    <td>{translations.attack}</td>
+                    <td>{this.props.combatParameters.get('attack')}</td>
+                  </tr>
+                  <tr>
+                    <td>{translations.shoot}</td>
+                    <td>{this.props.combatParameters.get('shoot')}</td>
+                  </tr>
+                  <tr>
+                    <td>{translations.defense}</td>
+                    <td>{this.props.combatParameters.get('defense')}</td>
+                  </tr>
+                </tbody>
+              </table>
+                <table className="table ability-table">
+                <tbody>
+                  <tr>
+                    <td>{translations.health}</td>
+                    <td>{this.props.combatParameters.get('health')}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
+          </div>
+
+          <div className="row">
+
+            <div className="col-xs-12">
+              <table className="table health-table">
+                <tbody>
+                  <tr>
+                    {Object.keys(healthArray).map(item =>
+                      <th key={item}>
+                        {healthArray[item]['content']}
+                      </th>
+                    )}
+                  </tr>
+
+                  {Object.keys(healthRowArray).map(item =>
+                    <tr key={item}>
+                      {Object.keys(healthArray).map(item =>
+                        <td key={item} className={healthArray[item]['active'] ? '' : 'inactive'}></td>
+                      )}
+                    </tr>
+                  )}
+
+                </tbody>
+              </table>
+            </div>
+
+          </div>
+
         </div>
-
-        <div className="col-xs-4">
-          <table className="table ability-table">
-            <tbody>
-              <tr>
-                <td>{translations.resistance}</td>
-                <td>{derivedAbilities.get('resistance')}</td>
-              </tr>
-              <tr>
-                <td>{translations.fortitude}</td>
-                <td>{derivedAbilities.get('fortitude')}</td>
-              </tr>
-              <tr>
-                <td>{translations.speed}</td>
-                <td>{derivedAbilities.get('speed')}</td>
-              </tr>
-              <tr>
-                <td>{translations.senses}</td>
-                <td>{derivedAbilities.get('senses')}</td>
-              </tr>
-              <tr>
-                <td>{translations.beauty}</td>
-                <td>{derivedAbilities.get('beauty')}</td>
-              </tr>
-              <tr>
-                <td>{translations.danger}</td>
-                <td>{derivedAbilities.get('danger')}</td>
-              </tr>
-              <tr>
-                <td>{translations.dignity}</td>
-                <td>{derivedAbilities.get('dignity')}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-
-        <div className="col-xs-4">
-          <table className="table ability-table">
-            <tbody>
-              <tr>
-                <td>{translations.combatSpeed}</td>
-                <td>{combatParameters.get('combatSpeed')}</td>
-              </tr>
-              <tr>
-                <td>{translations.attack}</td>
-                <td>{combatParameters.get('attack')}</td>
-              </tr>
-              <tr>
-                <td>{translations.shoot}</td>
-                <td>{combatParameters.get('shoot')}</td>
-              </tr>
-              <tr>
-                <td>{translations.defense}</td>
-                <td>{combatParameters.get('defense')}</td>
-              </tr>
-            </tbody>
-          </table>
-            <table className="table ability-table">
-            <tbody>
-              <tr>
-                <td>{translations.health}</td>
-                <td>{combatParameters.get('health')}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-
       </div>
-
-    </div>
-  </div>
-);
+    )
+  }
+}
 
 const CharacterSheet = connect(mapStateToProps)(ConnectedSheets);
 
