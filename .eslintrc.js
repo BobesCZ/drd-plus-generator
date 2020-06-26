cebe1841db3b4d84390d1e22f75292ae369dbd49
BobesCZ <bobes.chaos@gmail.com>
2020-06-26 13:28:25 +0200

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    semi: ['error', 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'react/prop-types': 'off',
+  },
+};
